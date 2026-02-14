d6c295e80b6cd9f6cca3698413ee1e3fc5ba6104
MNIKIEMA <nikiemamahamadi01@gmail.com>
2026-02-14 13:01:24 +0100

    update deps

--- tests/test_my_package.py
+++ tests/test_my_package.py
@@ -1,10 +1,10 @@
 from importlib.metadata import version
 
-from python_package_template import main
+from data_viewer import main
 
 
 def test_version():
-    assert version("python_package_template") == "0.1.0"
+    assert version("data-viewer") == "0.1.0"
 
 
 def test_main():
